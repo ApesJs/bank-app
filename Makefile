65604c41779694eee8dd86da43577d32cbda4788
Asep Jaenudin Sutarji <asepjaenudinsutarji@gmail.com>
2024-03-06 23:35:58 +0700

    making db migration and commands for docker and migrate

---
+++ Makefile
@@ -0,0 +1,16 @@
+postgres:
+	docker run --name postgres-app-bank -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=apesjs123 -d postgres
+
+createdb:
+	docker exec -it postgres-app-bank createdb --username=root --owner=root app-bank
+
+dropdb:
+	docker exec -it postgres-app-bank dropdb app-bank
+
+migrateup:
+	migrate -path db/migration -database "postgresql://root:apesjs123@localhost:5432/app-bank?sslmode=disable" -verbose up
+
+migratedown:
+	migrate -path db/migration -database "postgresql://root:apesjs123@localhost:5432/app-bank?sslmode=disable" -verbose down
+
+.PHONY: postgres createdb dropdb migrateup migratedown
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,32 @@
+CREATE TABLE "accounts"(
+                           "id" bigserial PRIMARY KEY,
+                           "owner" varchar not null,
+                           "balance" bigint not null,
+                           "currency" varchar not null,
+                           "created_at" timestamp not null default (now())
+);
+
+CREATE TABLE "entries"(
+                          "id" bigserial primary key,
+                          "account_id" bigint not null,
+                          "amount" bigint not null,
+                          "created_at" timestamp not null default (now())
+);
+
+CREATE TABLE "transfers"(
+                            "id" bigserial primary key,
+                            "from_account_id" bigint not null,
+                            "to_account_id" bigint not null,
+                            "amount" bigint not null,
+                            "created_at" timestamp not null default (now())
+);
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+CREATE INDEX ON "accounts" ("owner");
+CREATE INDEX ON "entries" ("account_id");
+CREATE INDEX ON "transfers" ("from_account_id");
+CREATE INDEX ON "transfers" ("to_account_id");
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
