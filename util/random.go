f52ce371e6e1b087b0ccae33f2d747a17de5ab45
Asep Jaenudin Sutarji <asepjaenudinsutarji@gmail.com>
2024-06-11 11:21:26 +0700

    add method for create transfer and add new table for users

--- util/random.go
+++ util/random.go
@@ -37,7 +37,7 @@ func RandomMoney() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "RP"}
+	currencies := []string{"IDR"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
